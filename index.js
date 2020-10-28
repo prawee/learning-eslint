f6bc2b19755e0feac93c21395ec2c67a11fbb2e5
Prawee Wongsa <prawee@ezgroup.com>
2020-10-28 07:03:58 +0700

    update

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-let didShowMessage = false;
+let didShowMessage = false
 
 function showMessage() {
-  const message = 'I like logs';
+  const message = 'I like logs'
 
   for (let i = 0; i < 5; i++) {
-// eslint-disable-next-line no-console
     console.log(message)
   }
 }
 
-showMessage();
+showMessage()
